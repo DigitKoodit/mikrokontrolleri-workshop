af71a6b4cca9a460e9c9c9dead4d7e1cd3ae71d9
Wa1tee <antti.auranen@iki.fi>
2019-01-19 22:33:18 +0200

    Fixed serial print formatting

--- sensor_node/sensor_node.ino
+++ sensor_node/sensor_node.ino
@@ -13,7 +13,7 @@
 #define I2C_ADDR 0x76 	// define the sensor i2c address
 #define SSID "" 		// Wifi SSID
 #define PASSWD "" 		// Wifi passwd
-#define IP "http://192.168.0.0:8080/newreading"			// Server IP-address
+#define IP "http://192.168.0.107:8080/newreading"			// Server IP-address
 #define NAME "TestiPalikka"			// Group name
 
 ESP8266WiFiMulti WiFiMulti;
@@ -55,7 +55,7 @@ void loop() {
 	{
 		HTTPClient http;
 
-		Serial.printf("[HTTP] begin...");
+		Serial.println("[HTTP] begin...");
 
 		http.begin(IP);
 		http.addHeader("Content-Type", "application/json");
