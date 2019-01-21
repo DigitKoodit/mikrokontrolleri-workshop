d60d69f70a6baa200bad5f78e571076adfc1a6c9
Antti Auranen <antti.auranen@iki.fi>
2019-01-21 15:39:49 +0200

    Minor adjustments

--- bme280_test/bme280_test.ino
+++ bme280_test/bme280_test.ino
@@ -4,13 +4,24 @@
 
 #include <Adafruit_BME280.h>
 #include <Wire.h>
+#include <ArduinoJson.h>
+#include <ArduinoJson.hpp>
 
-Adafruit_BME280 bme; //I2C
+#define I2C_ADDR 0x76 // define the sensor i2c address
+
+
+Adafruit_BME280 bme;
+
+char name[] = "KONSTA";
+
+
+const int capacity = JSON_OBJECT_SIZE(4);
+StaticJsonDocument<capacity> jd;
 
 void setup() {
 	Serial.begin(115200);
 	Wire.begin();
-    if (!bme.begin(0x76)) {  
+    if (!bme.begin(I2C_ADDR)) {  
     	Serial.println("Could not find a valid BME280 sensor, check wiring!");
     	while (1);
   	}else{
@@ -20,12 +31,25 @@ void setup() {
 }
 
 void loop() {
-	Serial.print("Temperature: ");
-	Serial.println(bme.readTemperature());
-	Serial.print("Pressure: ");
-	Serial.println(bme.readPressure());
-	Serial.print("Humidity: ");
-	Serial.println(bme.readHumidity());
+	// Serial.print("Temperature: ");
+	// Serial.println(bme.readTemperature());
+	// Serial.print("Pressure: ");
+	// Serial.println(bme.readPressure());
+	// Serial.print("Humidity: ");
+	// Serial.println(bme.readHumidity());
 
+	
+  	JsonObject data = doc.to<JsonObject>();
+  	
+  	data["name"] 		= name;
+  	data["temperature"]	= bme.readTemperature();
+  	data["pressure"] 	= bme.readPressure();
+  	data["humidity"]	= bme.readHumidity();
+
+  	String output;
+  	serializeJson(data, output);
+	JsonObject& obj = jb.parseObject(payload);
+	
+	
 	delay(1000);
 }
